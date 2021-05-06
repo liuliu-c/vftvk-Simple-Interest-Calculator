8a06a4d6ca30f0ebd88a87d72ba892aed339a333
liuliu-c <liuliu7960@gmail.com>
2021-05-06 03:34:01 +0000

    changes made from theia lab

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+
+    if (principal<=0){ // if the input value is not >0
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
+    }
+    else{
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * rate / 100;
+
+    var amount = interest + principal;
+
+    var year = new Date().getFullYear() + parseInt(years);
+
+    document.getElementById("result").innerHTML= "If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
+    }
     
+
 }
+
+function updateRate() //show updated rate when adjust bar
+    {
+
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * rate / 100;
+
+    var year = new Date().getFullYear() + parseInt(years);
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval +"%";
+
+     
+    }
         
